3f0866b344e51477ae014ca8e129d06d32a5ed5b
Heikkila Jari <jari.h.heikkila@tieto.com>
2017-11-04 14:29:28 +0200

    Add build script

---
+++ clone_and_build_ms_demo_apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p ~/git
+cd ~/git
+git clone https://github.com/jaheikki/microservice-demo-order-copy.git
+git clone https://github.com/jaheikki/microservice-demo-eureka-server-copy.git
+git clone https://github.com/jaheikki/microservice-demo-zuul-server-copy.git
+git clone https://github.com/jaheikki/microservice-demo-customer-copy.git
+git clone https://github.com/jaheikki/microservice-demo-catalog-copy.git
+
+cd ~/git/microservice-demo-order-copy && mvn clean install
+cd ~/git/microservice-demo-zuul-server-copy && mvn clean install
+cd ~/git/microservice-demo-eureka-server-copy && mvn clean install
+cd ~/git/microservice-demo-customer-copy && mvn clean install
+cd ~/git/microservice-demo-catalog-copy && mvn clean install
